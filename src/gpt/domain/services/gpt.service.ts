6930b8ad95bb86d6c5699122ee9b526e189c95ac
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2024-12-28 18:05:46 +0100

    feat: install openai package and create a prompt with Gemini model for orthography use case

--- src/gpt/domain/services/gpt.service.ts
+++ src/gpt/domain/services/gpt.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 import { ortographyCheckUseCase } from '../../orthography/application/ortography.use-case';
 import { OrthographyDto } from '../../orthography/domain/dto/orthography.dto';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: 'https://generativelanguage.googleapis.com/v1beta/openai/',
+  });
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await ortographyCheckUseCase({
+    return await ortographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
   }
--- src/gpt/orthography/application/ortography.use-case.ts
+++ src/gpt/orthography/application/ortography.use-case.ts
@@ -1,8 +1,42 @@
+import OpenAI from 'openai';
 import { OrthographyUseCaseParams } from '../domain';
 
+const FIRST_AI_RESPONSE = 0;
+
 export const ortographyCheckUseCase = async (
+  openai: OpenAI,
   options: OrthographyUseCaseParams,
 ) => {
   const { prompt } = options;
-  return { prompt };
+
+  const response = await openai.chat.completions.create({
+    model: 'gemini-1.5-flash',
+    messages: [
+      {
+        role: 'system',
+        content: `Te serán proveídos textos en español con posibles errores ortográficos y gramaticales. Tu tarea es corregirlos y retornar el texto corregido.
+          Debes responder en formato JSON como en el siguiente ejemplo:
+          Ejemplo de salida:
+          {
+            userScore: number, //  dar un porcentaje de acierto por el usuario
+            errors: string[], // ['error => solución']
+            message: string // Usa emojis y texto para felicitar al usuario'
+          }
+          Si no hay errores, retornar un mesaje de felicitación indicando al ususaio que no hay errores en el texto.
+          Las palabras usadas deben existir en el diccionario de la Real Academia Española.
+          `,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    temperature: 0.3,
+    max_tokens: 150,
+    response_format: {
+      type: 'json_object',
+    },
+  });
+
+  return response.choices[FIRST_AI_RESPONSE].message.content;
 };
